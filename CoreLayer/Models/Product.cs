d8b458e8ff15364b38d24a07edc08af61a01d79e
VantaMuammer <muammer.karaca@vantaworks.com>
2022-06-12 21:44:00 +0300

    Revert "Added Shared Layer"
    This reverts commit 092b87be155eab0456f20005b12e9b67ab36f7c7.

--- CoreLayer/Models/Product.cs
+++ CoreLayer/Models/Product.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace CoreLayer.Models
 {
-    public class Product:BaseModel<int>
+    public class Product
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price{ get; set; }
         public int Stock { get; set; }
