e2148971de1d628ca79f3bbe99101db243837b4a
VantaMuammer <muammer.karaca@vantaworks.com>
2022-06-12 21:47:51 +0300

    Added CoreLayer, DataLayer,SharedLayer

---
+++ SharedLibrary/SharedLayer/Dtos/ErrorDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLayer.Dtos
+{
+    public class ErrorDto
+    {
+        public List<string> Errors { get; set; } = new List<string>();
+        public bool IsShow { get; set; }
+
+        public ErrorDto(List<string> errors, bool isShow = true)
+        {
+            Errors = errors;
+            IsShow = isShow;
+        }
+
+        public ErrorDto(string error,bool isShow=true)
+        {
+            Errors.Add(error);
+            IsShow = isShow;
+        }
+    }
+}
---
+++ SharedLibrary/SharedLayer/Dtos/Response.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLayer.Dtos
+{
+    public class Response<TModel> where TModel :class
+    {
+        public bool IsSuccess { get; private set; }
+        public TModel Model { get; private set; }
+        public  int StatusCode { get; private set; } = 200;
+        public  ErrorDto Error { get; private set; }
+
+        public static Response<TModel> SuccessResponse(TModel model, int statusCode=200)
+        {
+            return new Response<TModel>() { IsSuccess = true, Model = model, StatusCode = statusCode };
+        }
+        public static Response<TModel> SuccessResponse( int statusCode = 200)
+        {
+            return new Response<TModel>() { IsSuccess = true, StatusCode = statusCode ,};
+        }
+        public static Response<TModel> ErrorResponse(ErrorDto errors,int statusCode=500)
+        {
+            return new Response<TModel>(){Error = errors,StatusCode = statusCode,IsSuccess = false};
+        }
+
+        public static Response<TModel> ErrorResponse(string error,bool isShow, int statusCode = 500)
+        {
+            return new Response<TModel>() {Error = new ErrorDto(isShow:false,error:error),StatusCode = statusCode,IsSuccess = false};
+        }
+    }
+}
