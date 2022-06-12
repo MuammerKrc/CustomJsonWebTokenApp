97a2d5878855666a32f72b4df0bcfd1f7ad0f15c
VantaMuammer <muammer.karaca@vantaworks.com>
2022-06-13 00:12:10 +0300

    Service Layer Sing Service Added

---
+++ ServiceLayer/Services/SignService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ServiceLayer.Services
+{
+    public static class SignService
+    {
+        public static SecurityKey GetSymmetricSecurityKey(string securityKey)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        }
+    }
+}
